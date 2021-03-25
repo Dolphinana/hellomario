d45d5e0478da6e464c088726b79347fe5277d386
Dolphinana <68866995+Dolphinana@users.noreply.github.com>
2021-03-25 08:31:50 +0100

    Hello Mario! Only few parts of Mario.

--- hellomario.asm
+++ hellomario.asm
@@ -8,15 +8,110 @@
 .byte $00
 .byte $00
 .byte $00
-.byte $00, $00, $00, $00, $00, ; filler bytes
+.byte $00, $00, $00, $00, $00 ; filler bytes
 .segment "ZEROPAGE" ; LSB 0 - FF
 .segment "STARTUP"
 Reset:
-    
+	SEI ; Disables all interrupts 
+	CLD ; disable decimal mode
+
+	; Disable sound IRQ
+	LDX #$40
+	STX $4017
+
+	; Initiallize the stack register
+	LDX #$FF
+	TXS
+
+	INX ; #$FF + 1 => #$0
+
+	; Zero out the PPU registers
+	STX $2000
+	STX $2001
+
+	STX $4010
+
+:
+	BIT $2002
+	BPL :-
+	
+	TXA
+
+CLEARMEM:
+	STA $0000, X ; $0000 => $00FF
+	STA $0100, X ; $0100 => $01FF
+	STA $0300, X ; $0300 => $03FF
+	STA $0400, X ; $0400 => $04FF
+	STA $0500, X ; $0500 => $05FF
+	STA $0600, X ; $0600 => $06FF
+	STA $0700, X ; $0700 => $07FF
+	LDA $FF	
+	STA $0200, X ; $0200 => $02FF
+	LDA $00
+	INX
+	BNE CLEARMEM
+; Wait for vblank
+:
+	BIT $2002
+	BPL :-
+	
+	LDA #$02
+	STA $4014
+	NOP
+
+	; $3F00
+	LDA #$3F
+	STA $2006
+	LDA $00
+	STA $2006
+
+	LDX $00
+
+LoadPalettes:
+	LDA PaletteData, X
+	STA $2007 ; $3F00, $3F01, $3F02 => 3F1F
+	INX
+	CPX #$20
+	BNE LoadPalettes
+
+	LDX #$00
+LoadSprites:
+	LDA SpriteData, X
+	STA $0200
+	INX
+	CPX #$20
+	BNE LoadSprites
+
+; Enable interrupts
+	CLI
+
+	LDA #%10010000	; enable NMI and change background to use second chr set of tiles ($1000
+	STA $2000	;store a into address 2000
+	; Enabling sprites and background for left-most 8 pixels
+	LDA #%00011110	;load A to binary 00011110
+	STA $2001	;STORE A TO 2001
+
 Loop:
 	JMP Loop
 
 NMI:
+	LDA #$02	;copy sprite data from $0200 => PPU memory for display
+	STA $4014	; store a to 4014
+	RTI
+
+PaletteData:
+  .byte $22,$29,$1A,$0F,$22,$36,$17,$0F,$22,$30,$21,$0F,$22,$27,$17,$0F	;background palette data
+  .byte $22,$16,$27,$18,$22,$1A,$30,$27,$22,$16,$30,$27,$22,$0F,$36,$17	;sprite palette data
+  
+SpriteData:
+  .byte $08, $00, $00, $08
+  .byte $08, $01, $00, $10
+  .byte $10, $02, $00, $08
+  .byte $10, $03, $00, $10
+  .byte $18, $04, $00, $08
+  .byte $18, $05, $00, $10
+  .byte $20, $06, $00, $08
+  .byte $20, $07, $00, $10
 
 .segment "VECTORS"
 	.word NMI
